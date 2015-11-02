4f59e3c9dee075ca4cc4b0f08710a0a70793e4f3
Matt0Hara <mohara2@worcester.edu>
2015-11-02 17:23:28 -0500

    added test

---
+++ spec/crumple_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe Crumple do
+  describe "#export_file" do
+    dump_file = "dummy.txt"
+    dump_dir = "/etc"
+    it "moves a file to the dump directory", fakefs: true do
+      FileUtils.touch(dump_file)
+      Dir.mkdir(dump_dir)
+      expect(File.exists?("dummy.txt")).to eq true
+      expect(Dir.exist?(dump_dir))
+    end
+
+  end
+end
